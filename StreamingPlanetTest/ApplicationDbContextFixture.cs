be5673f59e01d8dae3b09797886ab4ec84c4571c
jorgeOliveira1510 <jorge.oliveira15101999@gmail.com>
2023-02-05 16:39:14 +0000

    Criação de testes unitários para criação de utlizadores, autenticação e alteração da password.

---
+++ StreamingPlanetTest/ApplicationDbContextFixture.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using StreamingPlanet.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingPlanetTest
+{
+    public class ApplicationDbContextFixture : IDisposable
+    {
+        public ApplicationDbContext DbContext { get; private set; }
+
+        public ApplicationDbContextFixture()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlite(connection)
+                    .Options;
+            DbContext = new ApplicationDbContext(options);
+
+            DbContext.Database.EnsureCreated();
+
+            DbContext.SaveChanges();
+        }
+
+        public void Dispose() => DbContext.Dispose();
+    }
+}
